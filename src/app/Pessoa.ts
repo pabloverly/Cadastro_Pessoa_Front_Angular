6289289990528fed7eab124f02f716c5d3ca8fba
pablitoverly <pitterverly@gmail.com>
2023-03-29 14:09:15 -0300

    Frontend

---
+++ src/app/Pessoa.ts
@@ -0,0 +1,9 @@
+export class Pessoa {
+    pessoaId: number | undefined ;
+    nome: string | undefined;
+    sobrenome: string | undefined;
+    idade: number | undefined;
+    profissao: string | undefined;
+     
+    
+}
